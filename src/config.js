07606d8dda041e02c1b91f1f166b54d4e2e8c1bd
marklovely <marklovely67@gmail.com>
2026-07-28 17:55:27 +0100

    Revert "feat: add restore lights after movie button"
    This reverts commit affbfa538bde74b09c6591559694f47410f9f240.

--- src/config.js
+++ src/config.js
@@ -7,8 +7,7 @@ export const CONFIG = {
   buttons: [
     { id: 2, title: 'Bedtime', subtitle: 'Run bedtime scene', icon: '☾', colour: '#8b7cff' },
     { id: 6, title: 'Watch Movie', subtitle: 'Set up the lounge', icon: '▶', colour: '#d16dff' },
-    { id: 9, title: 'Restore Lights After Movie', subtitle: 'Restore lounge lighting', icon: '◉', colour: '#f4b64f' },
-    { id: 1, title: 'Downstairs On', subtitle: 'Turn lights on', icon: '●', colour: '#f4b64f' },
+    { id: 2, title: 'Downstairs On', subtitle: 'Turn lights on', icon: '●', colour: '#f4b64f' },
     { id: 5, title: 'Downstairs Off', subtitle: 'Turn lights off', icon: '○', colour: '#6f7b8f' },
     { id: 3, title: 'Garage Light On', subtitle: 'Turn garage light on', icon: '▣', colour: '#ff9f43' },
     { id: 4, title: 'Garage Light Off', subtitle: 'Turn garage light off', icon: '□', colour: '#6f7b8f' },
